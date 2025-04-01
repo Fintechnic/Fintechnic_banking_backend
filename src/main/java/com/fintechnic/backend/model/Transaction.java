a95dd1fd3127aa439cad52b8bfc9b2f0081c8649
andythienan <thienan18082006@gmail.com>
2025-04-01 20:59:34 +0700

    Noting fields in Transaction Model

--- src/main/java/com/fintechnic/backend/model/Transaction.java
+++ src/main/java/com/fintechnic/backend/model/Transaction.java
@@ -10,24 +10,29 @@ import java.time.LocalDateTime;
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long TransactionId;
+    Long TransactionId;
+
 
     @ManyToOne
     @JoinColumn(name = "user_id") // tạo ra một biến user_id trong Transaction có reference tới id của class User
     private User user;
 
+
+    //Field Tpe
     @Enumerated(EnumType.STRING)
     @Column
     private TransactionType type;
 
-
+    //Field Status
     @Enumerated(EnumType.STRING)
     @Column()
     private TransactionStatus status;
 
+    //Field Amnout
     @Column
     private Double amount;
 
+    //Field Description
     @Column
     private String description;
 
@@ -40,7 +45,5 @@ public class Transaction {
             this.status = TransactionStatus.PENDING; // Mặc định là PENDING
         }
     }
-
-   
 
 }
