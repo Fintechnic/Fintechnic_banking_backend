2b1dcd20153016ab0a8b2cf58c52173b07cfbaf0
Viet <phamcam102016@gmail.com>
2025-04-04 09:36:15 +0700

    Update ExpenseTrackingRepository

--- src/main/java/com/fintechnic/backend/repository/ExpenseTrackingRepository.java
+++ src/main/java/com/fintechnic/backend/repository/ExpenseTrackingRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ExpenseTrackingRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findByUserId(Long userId);
+    List<Transaction> findTransactionsByUserId(Long userId);
 }
