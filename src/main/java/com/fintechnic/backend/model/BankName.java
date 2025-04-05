939dcb8850296e8d75c175c125e65f3a4ca01eb9
andythienan <thienan18082006@gmail.com>
2025-04-05 12:17:47 +0700

    Update BankAccount Model

---
+++ src/main/java/com/fintechnic/backend/model/BankName.java
@@ -0,0 +1,35 @@
+package com.fintechnic.backend.model;
+
+public enum BankName {
+    ABBANK,         // Ngân hàng TMCP An Bình
+    ACB,            // Ngân hàng TMCP Á Châu
+    AGRIBANK,       // Ngân hàng Nông nghiệp và Phát triển Nông thôn Việt Nam
+    BAOVIETBANK,    // Ngân hàng TMCP Bảo Việt
+    BACABANK,       // Ngân hàng TMCP Bắc Á
+    BIDV,           // Ngân hàng TMCP Đầu tư và Phát triển Việt Nam
+    DONGABANK,      // Ngân hàng TMCP Đông Á
+    EXIMBANK,       // Ngân hàng TMCP Xuất Nhập khẩu Việt Nam
+    GPBANK,         // Ngân hàng Dầu khí Toàn cầu
+    HDBANK,         // Ngân hàng TMCP Phát triển TP.HCM
+    LIENVIETPOSTBANK,// Ngân hàng TMCP Bưu điện Liên Việt
+    LPBANK,         // Ngân hàng TMCP Bưu điện Liên Việt (tên cũ)
+    MSB,            // Ngân hàng TMCP Hàng Hải Việt Nam
+    NCB,            // Ngân hàng TMCP Quốc Dân
+    NAMABANK,       // Ngân hàng TMCP Nam Á
+    OCB,            // Ngân hàng TMCP Phương Đông
+    PGBANK,         // Ngân hàng TMCP Xăng dầu Petrolimex
+    PVCOMBANK,      // Ngân hàng TMCP Đại Chúng Việt Nam
+    SACOMBANK,      // Ngân hàng TMCP Sài Gòn Thương Tín
+    SCB,            // Ngân hàng TMCP Sài Gòn
+    SEABANK,        // Ngân hàng TMCP Đông Nam Á
+    SHB,            // Ngân hàng TMCP Sài Gòn - Hà Nội
+    TECHCOMBANK,    // Ngân hàng TMCP Kỹ Thương Việt Nam
+    TPBANK,         // Ngân hàng TMCP Tiên Phong
+    UOB,            // United Overseas Bank
+    VIETABANK,      // Ngân hàng TMCP Việt Á
+    VIETBANK,       // Ngân hàng TMCP Việt Nam Thương Tín
+    VIETCOMBANK,    // Ngân hàng TMCP Ngoại Thương Việt Nam
+    VIETINBANK,     // Ngân hàng TMCP Công Thương Việt Nam
+    VPBANK,         // Ngân hàng TMCP Việt Nam Thịnh Vượng
+    VIB             // Ngân hàng TMCP Quốc tế Việt Nam
+}
